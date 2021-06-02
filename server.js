83f3fc25519cb6cfa3be34e357fb1c11971e6c52
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-02 20:13:33 +0100

    added some styles and signup page

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const authRouter = require('./routes/authRouter');
 
+// Initialise app
 const app = express();
 
+// Set view engine
 app.set('views', './views');
 app.set('view engine', 'ejs');
+
+// Set static files
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('signup');
