f9f2b07a38ec3bc612ca795bafa1b5245b2eebc2
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-06 13:45:34 +0100

    started jwt auth

--- middleware/jwtMiddleware.js
+++ middleware/jwtMiddleware.js
@@ -1,9 +1,35 @@
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const UserModel = require('../models/userSchema');
 
-function generateToken(id){
-    const token = jwt.sign({id}, privateKey, {
+const privateKey = fs.readFileSync('../keys/private.key', 'utf8');
+const publicKey = fs.readFileSync('../keys/public.key', 'utf8');
+
+function generateToken(id) {
+    return jwt.sign({ id }, privateKey, {
         expiresIn: '24h',
+        algorithm: 'RS512'
     });
-
-    return token;
 }
+
+function verifyToken(req, res, next) {
+    const token = req.cookies.jwt;
+    if (token) {
+        jwt.verify(token, publicKey, async (err, decodedToken) => {
+            if (err) {
+                // Handle error
+                res.locals.user = null;
+                next();
+            } else {
+                const user = await UserModel.findById(decodedToken.id);
+                res.locals.user = user;
+                next();
+            }
+        });
+    } else {
+        res.locals.user = null;
+        next();
+    }
+}
+
+module.exports = { generateToken, verifyToken };
