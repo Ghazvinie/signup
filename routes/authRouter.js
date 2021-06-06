a6157668fef1a89e807759b12e92eea645551886
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-06-06 19:05:57 +0100

    started csrf

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,18 +1,21 @@
 const authRouter = require('express').Router();
 const { signUpGet, signUpPost, signInGet, signInPost, dashboardGet, changePasswordPost, signOutGet } = require('../controllers/authController');
 const { isUserAuth } = require('../middleware/jwtMiddleware');
+const csrf = require('csurf');
+
+const csrfProtection = csrf({cookie: false});
 
 authRouter.get('/signup', signUpGet);
 
-authRouter.post('/signup', signUpPost);
+authRouter.post('/signup', csrfProtection, signUpPost);
 
 authRouter.get('/signin', signInGet);
 
-authRouter.post('/signin', signInPost);
+authRouter.post('/signin', csrfProtection, signInPost);
 
 authRouter.get('/dashboard', isUserAuth, dashboardGet);
 
-authRouter.post('/changePassword', isUserAuth, changePasswordPost);
+authRouter.post('/changePassword', csrfProtection, isUserAuth, changePasswordPost);
 
 authRouter.get('/signout', signOutGet);
 
